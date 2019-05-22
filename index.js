b7b89fb99858f708d4fcfb6dd8380a82f5b929bb
Joseph Reed <josephreed2600@gmail.com>
2019-05-22 10:10:28 -0700

    rewrite index, apparently

--- index.js
+++ index.js
@@ -10,6 +10,54 @@ console.log('Starting ' + package.name + ' v' + package.version);
 // Configure logging for context 'main'
 logger = require('./logger').get('main');
 
+// Load classes and things
+const Community = require('./Community');
+const Participant = require('./Participant');
+const DiscordClient = require('./DiscordClient');
+const NullClient = require('./NullClient');
+
+// Load the definitions of participants and communities
+participants_config = require('./participants.json');
+communities_config = require('./communities.json');
+participants = {};
+communities = {};
+
+// Construct participants from list
+logger.info('Initializing Participants...');
+for(var p in participants_config) {
+	p = participants_config[p];
+	console.log(p);
+	switch(p.type) {
+		case 'discord': 
+			logger.info('Constructing DiscordClient: '+p.name);
+			participants[p.name] = (new DiscordClient(p.name, p.auth)).result;
+			logger.info('Constructed '+p.name);
+			break;
+		case 'irc':
+			logger.info('Ignoring request for IRC client [TODO]');
+			participants[p.name] = (new NullClient(p.name)).result;
+			logger.info('Constructed null client for '+p.name);
+			break;
+		default: break;
+	}
+}
+logger.info('Initialized Participants.');
+
+// Construct Communities
+logger.info('Initializing Communities...');
+for(var c in communities_config) {
+	c = communities_config[c];
+	console.log(c);
+	communities[c.id] = new Community(c.name, c.id);
+	for(var p in c.members) {
+		p = c.members[p];
+		console.log(p);
+		communities[c.id].add(participants[p]);
+	}
+}
+logger.info('Initialized Communities.');
+
+/*
 // Construct Discord client
 logger.info('Initializing Discord client...');
 discord_client = require('./discord_client');
@@ -32,3 +80,4 @@ community.add(irc_client);
 logger.info('...done.');
 
 // More clients and communities may be constructed as shown here
+/**/
